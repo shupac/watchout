5a7413be2ab39c0f0b4f3782377d246e6ff2ce0d
Hack Reactor Students <github@hackreactor.com>
2013-10-04 19:26:45 -0700

    Made player movable. Staggered enemy motion.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Global variables.
-var width = 600;
-var height = 400;
-var numEnemies = 5;
+var width = 700;
+var height = 500;
+var numEnemies = 20;
 var enemyRadius = 7;
+var moveInt = 1000;
 
 // Helper functions.
 var genEnemyLocations = function (){
@@ -15,7 +16,9 @@ var genEnemyLocations = function (){
 };
 
 var moveEnemies = function() {
-  enemies.data(genEnemyLocations).transition().attr('cy', function (d) { return d.y;})
+  enemies.data(genEnemyLocations)
+  .transition().duration(moveInt).delay(function(d) {return Math.random()*moveInt;})
+  .ease('linear').attr('cy', function (d) { return d.y;})
   .attr('cx', function (d) { return d.x;});
 };
 
@@ -33,4 +36,44 @@ var enemies = d3Canvas.selectAll('circle').data(genEnemyLocations)
   .attr('cx', function (d) { return d.x;})
   .style('fill', 'red');
 
-var enemyMover = setInterval(moveEnemies, 1000);
+var enemyMover = setInterval(moveEnemies, moveInt);
+
+var player = d3Canvas.selectAll('rect').data([{x:width/2, y:height/2}])
+  .enter()
+  .append('rect')
+  .attr('x', function (p) { return p.x;})
+  .attr('y', function (p) { return p.y;})
+  .attr('rx', 3)
+  .attr('ry', 3)
+  .attr('width', enemyRadius*2)
+  .attr('height', enemyRadius*2)
+  .style('fill', 'white');
+
+d3Canvas.on('mousemove', function(clickEvent) {
+  var coordinates = d3.mouse(this);
+  player.data([{x:coordinates[0], y:coordinates[1]}])
+        .attr('x', function (d) { return d.x;})
+        .attr('y', function (d) { return d.y;});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
